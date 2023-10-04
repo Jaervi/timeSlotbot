cba07f00abc052ac32e9697d0ce1260a678050ad
Aleksi Kuusinen <aleksi.kuusinen@aalto.fi>
2023-10-04 16:08:34 +0300

    Added two classes

---
+++ src/main/scala/s1/telegrambots/CalendarEvent.scala
@@ -0,0 +1,16 @@
+package s1.telegrambots
+
+class CalendarEvent (val startTime:String,val endTime:String):
+
+  def duration:Int=???
+
+  def startYear:Int=???
+
+  def startMonth:Int=???
+
+  def startDay:Int=???
+
+  def startHour:Int=???
+
+  def startMinute:Int=???
+
---
+++ src/main/scala/s1/telegrambots/FileHandler.scala
@@ -0,0 +1,16 @@
+package s1.telegrambots
+
+import java.util.Scanner
+import java.io.File
+
+//DTSTART:
+//DTEND:
+//DTSTART;VALUE=DATE
+//DTEND;VALUE=DATE
+
+
+object FileHandler {
+
+
+
+}
