2b7c354d0b0382b99844375778541a38f9f70afd
Aleksi Kuusinen <aleksi.kuusinen@aalto.fi>
2023-10-09 12:56:15 +0300

    Created Calendar

---
+++ src/main/scala/s1/telegrambots/Calendar.scala
@@ -0,0 +1,13 @@
+package s1.telegrambots
+
+import scala.collection.mutable.Buffer
+
+class Calendar(events:Buffer[CalendarEvent], val timeCreated:Int):
+
+  var eventList=events
+
+
+  def sortEventsByStartTime=
+    eventList=eventList.sortBy(_.startTimeInMinutes)
+
+  def printList=eventList.foreach(println)
